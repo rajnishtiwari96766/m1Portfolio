6941259a5f183cce5d47d144ca25a3ffea47205c
Rajnish Tiwari <rajnishtiwari96766@gmail.com>
2023-07-28 23:51:59 +0530

    first commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex justify-between items-center w-full h-20
+    text-white bg-black'>
+        <div>
+            <h1>Rajnish</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
