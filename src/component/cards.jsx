bea673fbe8ad9ce3ef7d7a9d1133f8221dfbd9be
Rajnish Tiwari <rajnishtiwari96766@gmail.com>
2023-08-03 05:44:29 +0530

    new commit

---
+++ src/component/cards.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Cards = () => {
+  return (
+    <div >
+    <div className='w-3/4 bg-white rounded shadow-lg'>
+            <img src='https://media.istockphoto.com/id/640164624/photo/hiding-behind-the-computer.jpg?s=170667a&w=0&k=20&c=Qg2QTvEjWJvL1Sfsb9qcQJXoMcKRWAPWQCTAUXM30cY='></img>
+        <div className='text-black font-bold text-3xl'>The coldest summer</div>
+        <div>
+            <p className='text-black'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, corrupti aspernatur. 
+            Iusto, commodi. Sunt rem repudiandae consequatur excepturi totam fugiat deserunt 
+            labore blanditiis animi nemo, autem laudantium, eveniet fugit assumenda!
+            </p>
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default Cards
--- src/component/experience.jsx
+++ src/component/experience.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
+import Cards from './cards'
 
 const Experience = () => {
   return (
+
     <div>
-        <div className= 'text-white'>
-            hii
+    <div className='flex p-5 m-2'>
+        
+        <div>
+        <Cards/>
         </div>
+        
+        <div>
+        <Cards/>
+        </div>
+    
+    </div>
+    <div className='flex items-center justify-center px-5 m-2'>
+        
+        <div>
+        <Cards/>
+        </div>
+        
+        <div>
+        <Cards/>
+        </div>
+    
+    </div>
     </div>
   )
 }
