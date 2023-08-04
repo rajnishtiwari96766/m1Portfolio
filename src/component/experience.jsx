7981ead545a9c3836998db425fa00322c93ed359
Rajnish Tiwari <rajnishtiwari96766@gmail.com>
2023-08-05 00:03:33 +0530

    new commit

--- src/component/experience.jsx
+++ src/component/experience.jsx
@@ -5,27 +5,10 @@ const Experience = () => {
   return (
 
     <div>
-    <div className='flex p-5 m-2'>
-        
+    <div className='flex p-5 m-2'>     
         <div>
         <Cards/>
         </div>
-        
-        <div>
-        <Cards/>
-        </div>
-    
-    </div>
-    <div className='flex items-center justify-center px-5 m-2'>
-        
-        <div>
-        <Cards/>
-        </div>
-        
-        <div>
-        <Cards/>
-        </div>
-    
     </div>
     </div>
   )
